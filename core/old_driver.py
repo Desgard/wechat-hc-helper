e3a151c7bbc3a2a26734c3e36e2f0dfd1cf3da0e
Harry Twan <gua@desgard.com>
2019-07-13 10:45:58 +0800

    update
    Signed-off-by: Harry Twan <gua@desgard.com>

--- core/old_driver.py
+++ core/old_driver.py
@@ -6,7 +6,7 @@ feeds = None
 rss_url = "https://github.com/SwiftOldDriver/iOS-Weekly/releases.atom"
 
 
-def fetch_old_driver_list() -> dict:
+def fetch_old_driver_list() -> list:
     global feeds
     if hasattr(ssl, '_create_unverified_context'):
         ssl._create_default_https_context = ssl._create_unverified_context
