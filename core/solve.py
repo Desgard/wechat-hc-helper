2a3782689442e4a006fb8e4030cabad4a3fbb64f
Harry Twan <gua@desgard.com>
2019-07-30 15:16:56 +0800

    变成手动触发每日一题
    Signed-off-by: Harry Twan <gua@desgard.com>

--- core/solve.py
+++ core/solve.py
@@ -22,69 +22,13 @@ related_group: Group = None
 if len(bot.groups().search(GROUP_NAME)) > 0:
     related_group = bot.groups().search(GROUP_NAME)[0]
 
-
-def valid_msg(msg):
-    '''
-    验证信息
-    :param msg:
-    :return:
-    '''
-    return '我要内推' in msg.text.lower()
+guagua = bot.friends().search("冬瓜")
 
 
-def invite(user):
-    '''
-    邀请用户方法
-    :param user: User 对象
-    :param bot: Bot 对象
-    :return:
-    '''
-    group = bot.groups().search("")
-    user.send(group.name)
-    if len(group) > 0:
-        group[0].add_members(user, use_invitation=True)
-
-
-@bot.register(msg_types=FRIENDS)
-def new_friends(msg):
-    '''
-    处理加好友
-    :param msg:
-    :return:
-    '''
-    user = msg.card.accept()
-    if valid_msg(msg):
-        invite(user)
-    else:
-        user.send('hello {user_name},忘记填写加群口令了,去填写吧'.format(user_name=user.name))
-
-
-@bot.register(Friend, msg_types=TEXT)
-def exist_friends(msg):
-    """
-    处理邀请加群信息
-    :param msg:
-    :return:
-    """
-    if valid_msg(msg):
-        msg.sender.send('点击加群')
-        group = bot.groups().search(GROUP_NAME)
-        if len(group) <= 0:
-            msg.sender.send('读取群信息失败')
-        else:
-            msg.sender.send(group[0].name)
-            gp = group[0]
-            len_mem = len(gp.members)
-            global related_group
-            related_group = gp
-            if len_mem >= 100:
-                # gp.add(users=[msg.sender], use_invitation=True)
-                msg.sender.send("自动拉好友接口失效，等待手动拉好友。")
-            else:
-                msg.sender.send_image(os.path.join(os.getcwd(), 'qrcode.jpg'))
-
-    else:
-        msg.sender.send('hello {user_name},忘记填写加群口令了,去填写吧'.format(user_name=msg.sender.name))
+@bot.register(guagua, msg_types=TEXT)
+def _111(msg):
+    if str(msg.text).lower() == "guaguagua":
+        send_news()
 
 
 @bot.register(bot.groups(), msg_types=TEXT)
@@ -250,5 +194,4 @@ def random_answer(msg):
         msg.sender.send(f"了解一下：{car[s]} ?? ")
 
 
-send_news()
 embed()
